6435dbfb96f19e5866b281725ca445cc6814187f
Konstantin <kostet2033@gmail.com>
2021-11-27 20:07:32 +0200

    HW8

---
+++ HW8/Task_2/EnumColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    enum EnumColor : int
+    {
+        Green = 1,
+        Blue = 2,
+        Red = 3,
+        Yellow = 4,
+        Black = 5,
+        White = 6,
+        Gray = 7
+    }
+}
---
+++ HW8/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your msg: ");
+            ColorPrint.Print("SPAM!!!", (int)EnumColor.Green);
+            ColorPrint.Print("SPAM!!!", (int)EnumColor.Black);
+            ColorPrint.Print("SPAM!!!", (int)EnumColor.Blue);
+            ColorPrint.Print("SPAM!!!", (int)EnumColor.Gray);
+            ColorPrint.Print("SPAM!!!", (int)EnumColor.Red);
+            ColorPrint.Print("SPAM!!!", (int)EnumColor.White);
+            ColorPrint.Print("SPAM!!!", (int)EnumColor.Yellow);
+        }
+    }
+}
