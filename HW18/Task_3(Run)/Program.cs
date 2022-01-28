d865b37964ac5c864c4123a7333ce57da9ea5d16
Konstantin <kostet2033@gmail.com>
2022-01-28 02:21:36 +0200

    Add HW15-18

--- HW18/Task_3(Run)/Program.cs
+++ HW18/Task_3(Run)/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using MyNameSpace;
+using Task_3_lib_;
 
-namespace Task_1
+namespace Task_3_Run_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MyClass.Method();
+            Class1 run = new Class1();
+
+            run.SomeMethod();
         }
     }
 }
-
-
