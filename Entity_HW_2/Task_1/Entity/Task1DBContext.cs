9caceb7709945612192fd9311a3279275e321201
Konstantin <kostet2033@gmail.com>
2022-03-23 02:02:12 +0200

    Add HW2, DB and User Secrets

---
+++ Entity_HW_2/Task_1/Entity/Task1DBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Task_1.Entity
+{
+    public class Task1DBContext : DbContext 
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Curse> Curses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Task1DBContext>()
+                .Build();
+
+            string connectStr = builder.GetConnectionString("ConnectDBTask1");
+
+            optionsBuilder.UseSqlServer(connectStr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ConfiguringCurse());
+            modelBuilder.ApplyConfiguration(new ConfiguringAuthor());
+        }
+    }
+}
---
+++ Entity_HW_2/Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Task_1.Entity;
+
+namespace Task_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
